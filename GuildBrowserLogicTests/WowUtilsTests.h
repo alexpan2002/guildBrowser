3a66d053628f78753065a7889f4553d6b7b140f2
Alex Pan <alexpa@Alexs-Mac-mini.local>
2013-08-10 11:47:01 +0800

    Add first guildBrowserLogicTests test suite

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Alex Pan on 13/8/10.
+//  Copyright (c) 2013年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
